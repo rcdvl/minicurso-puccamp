0c844e3e974f5e78e98a490e726544b2b4ac061b
Renan Cadaval <renancadaval@gmail.com>
2019-08-23 10:39:59 -0300

    Initial commit

---
+++ src/main/java/br/org/sidi/puccamp/minicurso/MinicursoApplication.java
@@ -0,0 +1,13 @@
+package br.org.sidi.puccamp.minicurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinicursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinicursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/sidi/puccamp/minicurso/MinicursoApplicationTests.java
@@ -0,0 +1,16 @@
+package br.org.sidi.puccamp.minicurso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinicursoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
